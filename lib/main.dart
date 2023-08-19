9eb9f7a0e8a22d1f43e8d82bd57211901e442a24
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-08-19 09:55:40 +0100

    added a stateful widget to the home_page file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'pages/home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomePage(),
     );
   }
